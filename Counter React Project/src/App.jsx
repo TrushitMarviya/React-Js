65ce67bf5b6e991c03d2a603fed97f293ada4f6e
TrushitMarviya <marviyatrushit0@gmail.com>
2024-09-04 16:19:06 +0530

    project

---
+++ Counter React Project/src/App.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Counter from './Components/Counter'
+
+export default function App() {
+  return (
+    <div>
+      <Counter/>
+    </div>
+  )
+}
---
+++ Counter React Project/src/Components/Counter.jsx
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState } from 'react'
+
+export default function Counter() {
+    const[count , setCount]=useState(0)
+    const increase = () =>{
+        setCount(count + 1)
+    } 
+    const decrease = () =>{
+        setCount(count - 1)
+    }
+    const reset = () =>{
+        setCount(0)
+    }
+  return (
+    <div className='container'>
+    <section className="cd-intro">
+		<h1 className="cd-headline push">
+			<span style={{color:"white"}}>Counter </span>
+			<span className="cd-words-wrapper">
+				<b className="is-visible" style={{color:"red"}}>App</b>
+				<b style={{color:"red"}}>Project</b>
+			</span>
+		</h1>
+	</section> 
+    <p className="counter">{count}</p>
+    <button className="increase" onClick={increase} >+</button>
+    <button className="decrease" onClick={decrease}>-</button>
+    <button className="reset" onClick={reset}>0</button>
+    </div>
+  )
+}
