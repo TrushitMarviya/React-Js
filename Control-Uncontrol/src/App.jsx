230206a2061c8407c1339e0e325b81a1ed6303ca
TrushitMarviya <marviyatrushit0@gmail.com>
2024-09-13 11:10:13 +0530

    Control-Uncontrol

---
+++ Control-Uncontrol/src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Control from './Components/Control'
+import Uncontrolled from './Components/Uncontrolled'
+
+export default function App() {
+  return (
+    <>
+      <Control></Control>
+      <Uncontrolled></Uncontrolled>
+    </>
+  )
+}
