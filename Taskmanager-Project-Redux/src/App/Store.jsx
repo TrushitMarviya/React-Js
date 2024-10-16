b04cb79467890fdd06fed4f97aba761fff7dc0ee
TrushitMarviya <162434195+TrushitMarviya@users.noreply.github.com>
2024-10-16 15:19:41 +0530

    Delete Taskmanager-Project-Redux directory

--- Taskmanager-Project-Redux/src/App/Store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import  count  from "../Configure/TaskSlice";
-
-export const store = configureStore({
-    reducer: {
-        taskKey:count
-    }
-})
